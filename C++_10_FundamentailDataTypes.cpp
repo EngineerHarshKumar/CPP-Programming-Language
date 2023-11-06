8aa9b806fbf94d94c852038ee55b772e553a4648
EngineerHarshKumar <harshkumarsoftwareengineer@gmail.com>
2023-11-06 18:30:25 +0530

    UPloading Fundamental file

---
+++ C++_10_FundamentailDataTypes.cpp
@@ -0,0 +1,43 @@
+/*
+Smallest unit of memory is binary digit ( 1 or 0 ), which can hold the value of 1 or 0.
+Memory is organized into a sequencially unit which called the memory address, if you don't understand it learn this one 
+Memory address is unique identifier for the location in computer's memory where the data can be store ...The memory address is used to manipulate the data at that location 
+
+NOTE: In mordern computer, each bit does not have the memory address because memory address is limited and access data bit by bit is rare so that's why every bit does not have memory address, In mordern computer system collection of 8 bits ( called byte ) have the memory adddress.., C++ work on byte sized..
+
+DATA-TYPES
+The data types is basically used for this 3 things
+1. Tell the computer how to interpret data ( bits or bytes ) 
+2. Define the size of that variable ( how many bits are reserved for that variable ) 
+3. Detemine which kind of data that specific variable can store 
+
+C++ Comes with many build-in support of data types wich called FUNDAMENTAL DATA TYPES 
+
+float  - Floating point 
+double - Floating point 
+long double - Floating point
+bool - Integral ( boolean value ) 
+char - Integral ( character value ) 
+As many more 
+
+
+NOTE: Integer and Integral similar but have different meaning 
+In mathematics , Integer is a number with no fractional and decimal part, it include positive and negative number including zero 
+In C++ , Integer is also same which will be declared using 'int' keyword and integer in c++ have many other to declared it 
+for example short, int , long , long long and theri also unsigned and signed declaration ways....
+
+
+The Integral term mean "Like a Integer", Most the integral is used as a part of 'integral type', its will considered as its show that its store as a integer or its a integer typse ...
+
+In mordern programming language their is an type 'string' which hold the sequence of character to show the string But In C++ 'string' is not a fundamental datatype instead its a COMPOUND TYPE
+*/
+
+#include <iostream>
+#include <cstdlib>
+
+int main() {
+
+
+
+    return EXIT_SUCCESS ;
+}
